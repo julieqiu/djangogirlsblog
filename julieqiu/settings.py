76f1cfa7566af5d8861b57d222ca438d258c6aad
Julie <julieyeqiu@gmail.com>
2016-02-27 15:06:24 -0500

    Changed the HTML for the site.

--- julieqiu/settings.py
+++ julieqiu/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'blog',
+    'blog'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -113,8 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 
 
 # Static files (CSS, JavaScript, Images)
